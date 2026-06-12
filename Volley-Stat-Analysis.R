2c7a553db5cff95e1f2889811738337831f1c247
Akshaya Nair <nakshaya@mail.uni-paderborn.de>
2026-06-13 01:53:56 +0200

    winner-loser plot , winner of each set calculation

--- Volley-Stat-Analysis.R
+++ Volley-Stat-Analysis.R
@@ -1,12 +1,14 @@
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+
 #loading the data and check for missing values
+setwd(getwd())
 matchstatsdata <- read.csv("matchStats.csv",header = TRUE,  sep = ";",dec = ".")
+
+# Check for columns with missing values
 colSums(is.na(matchstatsdata))
-summary(matchstatsdata)
+#summary(matchstatsdata)
 
+# Adding column Home.Team_won to indicate whether the home team won 
 matchstatsdata$Home.Team_won <- ifelse(matchstatsdata$Home.Team == matchstatsdata$Winner, 1, 0)
-
-
-#analyzing the kills.home
-qqnorm(matchstatsdata$Kills.Home)
-qqline(matchstatsdata$Kills.Home, col="red")
-shapiro.test(matchstatsdata$Kills.Home) # results in W = 0.95812, p-value = 0.001134
