d6629c98f6f1e42b28c6ec224cd228b9b4b01e08
Akshaya Nair <nakshaya@mail.uni-paderborn.de>
2026-06-22 23:14:00 +0200

    git push

--- Volley-Stat-Analysis.R
+++ Volley-Stat-Analysis.R
@@ -1,7 +1,11 @@
+install.packages("corrplot")
+library(corrplot)
 library(ggplot2)
 library(tidyverse)
 library(dplyr)
 library(GGally)
+library(MASS)
+library(mice)
 
 #loading the data and check for missing values
 setwd(getwd())
@@ -9,70 +13,55 @@ matchstatsdata <- read.csv("DataSet/matchStats.csv",header = TRUE,  sep = ";",de
 colSums(is.na(matchstatsdata))
 
 
-#Receptions.Home,Set4.Home,Set4.Away,Set5.Home,Set5.Away have NA values
-#Set4 and Set5  Could be the case of MAR since the missing data occurs 
+#The column that have NA values are: Receptions.Home,Set4.Home,Set4.Away,Set5.Home,Set5.Away 
+#Set4 and Set5  Could be the case of MNAR since the missing data occurs 
 #for data entries where the winner is selected based on scores of previous three sets
 
 #Receptions.Home could be the case of MCAR because the value does not depend on any variables
 
+#since the matches were not played replacing NA values to 0
+sets_mnar <- c("Set4.Home","Set4.Away","Set5.Home","Set5.Away")
+
+matchstatsdata[sets_mnar] <- lapply(matchstatsdata[sets_mnar],
+                                    function(x) ifelse(is.na(x), 0, x))
+colSums(is.na(matchstatsdata))
 
 # Adding column Home.Team_won to indicate whether the home team won 
 matchstatsdata$Home.Team_won <- ifelse(matchstatsdata$Home.Team == matchstatsdata$Winner, 1, 0)
 
-home_team = table(matchstatsdata$Home.Team)
-away_team = table(matchstatsdata$Away.Team)
-winner = table(matchstatsdata$Winner)
-losser = table(matchstatsdata$Loser)
+# Select only numeric variables for imputation model
+num_data <- matchstatsdata[, sapply(matchstatsdata, is.numeric)]
+
+# Run multiple imputation with PMM
+imp <- mice(num_data, 
+            m = 5,          # number of imputed datasets
+            method = "pmm", # predictive mean matching
+            seed = 123)
+completed_data <- complete(imp, 1)   # choose imputed dataset 1
+matchstatsdata$Receptions.Home <- completed_data$Receptions.Home
 
 
-plot(home_team)
-barplot(away_team)
-barplot(winner)
-barplot(losser)
-
-matchstatsdata$Set1_Home_Win <- ifelse(
-  matchstatsdata$Set1.Home - matchstatsdata$Set1.Away >= 2,
-  1,
-  0
-)
-matchstatsdata$Set2_Home_Win <- ifelse(
-  matchstatsdata$Set2.Home - matchstatsdata$Set1.Away >= 2,
-  1,
-  0
-)
-matchstatsdata$Set3_Home_Win <- ifelse(
-  matchstatsdata$Set3.Home - matchstatsdata$Set1.Away >= 2,
-  1,
-  0
-)
-matchstatsdata$Set4_Home_Win <- ifelse(
-  matchstatsdata$Set4.Home - matchstatsdata$Set1.Away >= 2,
-  1,
-  0
-)
-matchstatsdata$Set5_Home_Win <- ifelse(
-  matchstatsdata$Set5.Home - matchstatsdata$Set1.Away >= 2,
-  1,
-  0
-)
 #outlier detection
 ggplot(matchstatsdata,aes(x=Kills.Home,y=Home.Team))+geom_boxplot() 
-ggplot(matchstatsdata,aes(x=Blocks.Home,y=Home.Team))+geom_boxplot()
-ggplot(matchstatsdata,aes(x=Set1.Home,y=Home.Team))+geom_boxplot()
-ggplot(matchstatsdata,aes(x=Set2.Home,y=Home.Team))+geom_boxplot()
-ggplot(matchstatsdata,aes(x=Set3.Home,y=Home.Team))+geom_boxplot()
-ggplot(matchstatsdata,aes(x=Set4.Home,y=Home.Team))+geom_boxplot()
-ggplot(matchstatsdata,aes(x=Set5.Home,y=Home.Team))+geom_boxplot()
 
-# sapply(colnames(matchstatsdata),function(x){
-#   ggplot(matchstatsdata,aes(x=x,y=Home.Team))+geom_boxplot()
-# })
+num_cols <- names(matchstatsdata)[sapply(matchstatsdata, is.numeric)]
+
+sapply(num_cols, function(col) {
+  print(
+    ggplot(matchstatsdata, aes(x = .data[[col]], y = Home.Team)) +
+      geom_boxplot() +
+      ggtitle(col) +
+      theme_bw()
+  )
+})
+
 
 #correlation
-df_num=matchstatsdata[, sapply(matchstatsdata, is.numeric)]
-cormatrix <- cor(df_num, use = "pairwise.complete.obs")
-cormatrix
-corrplot(cormatrix, method = "color")
+# df_num=matchstatsdata[, sapply(matchstatsdata, is.numeric)]
+# cormatrix <- cor(df_num, use = "pairwise.complete.obs")
+# cormatrix
+# corrplot(cormatrix, method = "color")
+
 
 #normality Detection for one variable Kills.Home
 q_empirical <- sort(matchstatsdata$Kills.Home)
@@ -93,7 +82,35 @@ sapply(colnames(numeric_cols), function(x) {
   
   qqnorm(vec, main = x, pch = 19, cex.lab = 1.5, cex.main = 1.5, ylab = "")
   qqline(vec, lwd = 2, col = "red")
+  
 })
+
+
+
+
+
+
+
+
+
+
+
+
+#set3.Away 
+#homecountry is Argentina has outlier 37 which can be genuine but was a long match
+#homecountry is Poland has outlier has lowest 14 which can be genuine 
+#homecountry is Turkey has outlier 22 and 29 which can be genuine
+#homecountry is slovenia has outlier 22 and 29 which can be genuine
+
+#Reception.Away
+#lowest reception is serbia and they lost the game
+
+#reception.Home
+#Usa has highest 87 and they won against IRAN
+
+#Digs.Away
+# Serbia digs.away has highest of 106 digs still serbia lost slovania
+#Serbia digs.away has lowest of 29 still serbia lost
 
 
 
